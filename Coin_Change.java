f140826a39fc36a4caa0a3a28844106a1df2531f
Atishay-Garg21 <atishaygarg11@gmail.com>
2025-07-07 13:15:12 +0530

    Second commit

---
+++ Coin_Change.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Coin_Change {
+    public static int minCoins(int[] coins, int sum) {
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= sum; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[sum] == Integer.MAX_VALUE ? -1 : dp[sum];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+        int sum = sc.nextInt();
+        int result = minCoins(coins, sum);
+        System.out.println( result);
+    }
+}
